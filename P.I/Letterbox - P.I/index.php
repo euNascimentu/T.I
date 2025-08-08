6daaf30889ec8f2f61ee149dd34d3a90c5b6c3a2
Fernando <164927005+euNascimentu@users.noreply.github.com>
2025-08-07 23:19:20 -0300

    .

---
+++ P.I/Letterbox - P.I/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> GameCut, Home! </title>
+    <link rel="stylesheet" href="style/index.css">
+</head>
+<body>
+    <div id="container">
+        <header>
+            <img src="source/logoRoxa-remove.png" alt="">
+        </header>
+        <section>
+            estou aqui
+        </section>
+    </div>
+</body>
+</html>
