39cc98be2773acf771e592351a9945e52f3ced35
Fernando <164927005+euNascimentu@users.noreply.github.com>
2025-08-11 17:21:04 -0300

    .

--- P.I/Letterbox - P.I/index.php
+++ P.I/Letterbox - P.I/index.php
@@ -5,15 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> GameCut, Home! </title>
     <link rel="stylesheet" href="style/index.css">
+    <style>
+        /*Press start 2p*/
+        @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Press+Start+2P&display=swap');
+        /*Bungee*/
+        @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Bungee&family=Press+Start+2P&display=swap');
+    </style>
 </head>
 <body>
-    <div id="container">
         <header>
-            <img src="source/logoRoxa-remove.png" alt="">
+            <div id="fundologo">
+                <img class="logo" src="source/logoRoxa-Remove.png" alt="">
+            </div>
+            <div id="fundoButtons">
+                <a class="a" href="">.um</a>
+                <a class="a" href="">.dois</a>
+                <a class="a" href="">.tres</a>
+            </div>
         </header>
+        <div id="Banner">
+            <img class="fotoBan" src="source/banner.gif" alt="">
+        </div>
         <section>
-            estou aqui
+            <p>Lançamentos</p>
         </section>
-    </div>
 </body>
 </html>
