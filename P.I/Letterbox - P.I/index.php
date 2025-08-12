84f2162dc1004b0ecd92d63a9fef419a2e775008
Fernando <164927005+euNascimentu@users.noreply.github.com>
2025-08-12 17:02:44 -0300

    .

--- P.I/Letterbox - P.I/index.php
+++ P.I/Letterbox - P.I/index.php
@@ -1,7 +1,32 @@
 <?php
 session_start(); // Sempre iniciar a sessão antes de qualquer HTML
-?>
 
+// ===== Conexão com o banco =====
+$host = "localhost";      // Servidor do banco
+$usuario = "root";        // Usuário MySQL
+$senha = "";              // Senha MySQL (troque se tiver)
+$banco = "PIBD";          // Nome do banco
+
+$conn = new mysqli($host, $usuario, $senha, $banco);
+
+// Testar conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8"); // Evitar problemas com acentos
+
+// ===== Buscar todos os usuários =====
+$sql = "SELECT * FROM Usuario";
+$result = $conn->query($sql);
+
+$usuarios = [];
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $usuarios[] = $row;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="PT-BR">
 <head>
@@ -16,68 +41,96 @@ session_start(); // Sempre iniciar a sessão antes de qualquer HTML
         @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Bungee&family=Press+Start+2P&display=swap');
     </style>
 </head>
-<body>
+<body class="bg-animado">
         <header>
             <div id="fundologo">
                 <img class="logo" src="source/logoRoxa-Remove.png" alt="">
             </div>
             <div id="fundoButtons">
-                <a class="a" href="">.um</a>
-                <a class="a" href="">.dois</a>
-                <a class="a" href="">.tres</a>
-                <div id="perfilLogin">
-                    <?php if (isset($_SESSION['usuario'])): ?>
-                <!-- Usuário logado -->
-                <a href="perfil.php">perfil</a>
-                    <?php else: ?>
-                        <!-- Usuário NÃO logado -->
-                        <a href="login.html">login</a>
-                    <?php endif; ?>
+                <div class="botoesHeader">
+                    <a class="a" href="">.um</a>
+                    <a class="a" href="">.dois</a>
+                    <a class="a" href="">.tres</a>
                 </div>
+                <div class="perfilLogin">
+                <?php if (isset($_SESSION['usuario'])): ?>
+                <div class="dropdown">
+                    <button class="dropbtn"><?= htmlspecialchars($_SESSION['usuario']['nome']) ?></button>
+                        <div class="dropdown-content">
+                            <a href="perfil.php">Perfil</a>
+                            <a href="logout.php">Sair</a>
+                        </div>
+                </div>
+                <div class="fotoPerfil">
+                    <img class="fotinhaPerfil" src="source/marcosFOTO.jpg" alt="">
+                </div>
+                <?php else: ?>
+                    <!-- Usuário NÃO logado -->
+                    <a href="login.php">login</a>
+                <?php endif; ?>
             </div>
         </header>
         <div id="Banner">
             <img class="fotoBan" src="source/banner.gif" alt="">
         </div>
+
         <section>
             <p class="tituloConteudo">Lançamentos</p>
             <article>
                 <div id="Cards">
-                    <div class="fundoFotoCard">
-                        <img class="fotoCard" src="source/gpt.png" alt="">
-                    </div>
-                    <div class="textoCard">
-                        estou aqui
-                    </div>
+                    <a href="">
+                        <div class="fundoFotoCard">
+                            <img class="fotoCard" src="source/gpt.png" alt="">
+                        </div>
+                        <div class="textoCard">
+                            estou aqui
+                        </div>
+                    </a>
                 </div>
 
                 <div id="Cards">
-                    <div class="fundoFotoCard">
-                        <img class="fotoCard" src="source/gpt.png" alt="">
-                    </div>
-                    <div class="textoCard">
-                        estou aqui
-                    </div>
+                    <a href="">
+                        <div class="fundoFotoCard">
+                            <img class="fotoCard" src="source/gpt.png" alt="">
+                        </div>
+                        <div class="textoCard">
+                            estou aqui
+                        </div>
+                    </a>
                 </div>
 
                 <div id="Cards">
-                    <div class="fundoFotoCard">
-                        <img class="fotoCard" src="source/gpt.png" alt="">
-                    </div>
-                    <div class="textoCard">
-                        estou aqui
-                    </div>
+                    <a href="">
+                        <div class="fundoFotoCard">
+                            <img class="fotoCard" src="source/gpt.png" alt="">
+                        </div>
+                        <div class="textoCard">
+                            estou aqui
+                        </div>
+                    </a>
                 </div>
 
                 <div id="Cards">
-                    <div class="fundoFotoCard">
-                        <img class="fotoCard" src="source/gpt.png" alt="">
-                    </div>
-                    <div class="textoCard">
-                        estou aqui
-                    </div>
+                    <a href="">
+                        <div class="fundoFotoCard">
+                            <img class="fotoCard" src="source/gpt.png" alt="">
+                        </div>
+                        <div class="textoCard">
+                            estou aqui
+                        </div>
+                    </a>
                 </div>
             </article>
         </section>
 </body>
+<script>
+setTimeout(() => {
+  document.body.classList.remove('bg-animado');
+  document.body.classList.add('bg-estatico');
+}, 14500);
+
+</script>
 </html>
+<?php
+$conn->close(); // Fechar conexão
+?>
